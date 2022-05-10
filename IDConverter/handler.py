e5570b428663f1acd0787525e45ff6855d28a3b8
Luca Falavigna <dktrkranz@debian.org>
2022-05-10 14:40:45 +0200

    Parse names with spaces and prefixes

--- IDConverter/handler.py
+++ IDConverter/handler.py
@@ -1,4 +1,4 @@
-from re import match
+from re import match, sub
 
 
 def main(event, context):
@@ -9,6 +9,10 @@ def main(event, context):
         if match('^[A-Z]{2}[A-Z_][A-Z]{2}[A-Z_][0-9ASX][0-9]$', user):
             return user
         else:
+            if len(surname.split()) > 1:
+                surname = ' '.join([x for x in surname.split() if len(x) > 3]
+                                   or surname.split()[-1:])
+            name, surname = (sub('[^A-Z0-9]', '', n) for n in (name, surname))
             return f'{surname[:3].ljust(3, "_")}{name[:3].ljust(3, "_")}90'
     else:
         return 'ERROR'
